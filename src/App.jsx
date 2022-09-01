5ad791eb5f434a874677ac906c662f876acc1a37
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-01 21:37:21 +0600

    complete intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Intro } from './components/Intro';
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Intro />
     </>
   );
 };
